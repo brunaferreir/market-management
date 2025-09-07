86d1d660737568fef604e2e0d8ac452b367711a2
Bruna <bruna.fsilva@aluno.faculdadeimpacta.com.br>
2025-09-07 14:57:34 -0300

    feat(vendedores): Adiciona fluxo completo de registro, ativação e login

--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,5 @@
 from src.Application.Controllers.user_controller import UserController
+from src.Application.Service.user_service import UserService  
 from flask import jsonify, make_response, request, redirect, url_for
 from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, jwt_required
 
@@ -9,41 +10,43 @@ def init_routes(app):
             "mensagem": "API - OK; Docker - Up",
         }), 200)
     
-#-AQUI----------------------  POST CRIA UM NOVO USUARIO
-    @app.route('/user', methods=['POST'])
-    def register_user():
-        return UserController.register_user()
+#----------------------  GET PARA 1 SELLER
+    @app.route('/api/sellers/<int:user_id>', methods=['GET'])
+    def get_seller_by_id(user_id):
+        return UserController.get_seller_by_id(user_id)  
     
 
-#-AQUI----------------------  GET PARA 1 USUARIO
-    @app.route('/user/<int:user_id>', methods=['GET'])
-    def get_user_by_id(user_id):
-        return UserController.get_user(user_id)
+#----------------------  POST CRIA UM SELLER
+    @app.route('/api/sellers', methods=['POST'])
+    def create_seller():
+        return UserController.create_seller()    
 
+#----------------------  POST ATIVA SELLER COM CÓDIGO
+    @app.route('/api/sellers/activate', methods=['POST'])
+    def ativar_seller():
+        return UserController.activate_seller()
+    
+#----------------------  POST LOGIN SELLER
+    @app.route('/api/auth/login', methods=['POST'])
+    def login_seller():
+        return UserController.login_seller()    
 
-#-AQUI----------------------  PUT ATUALIZA 1 USUARIO
+#-----------------------  PUT ATUALIZA 1 USUARIO
     @app.route('/user/<int:user_id>', methods=['PUT'])
     def update_user(user_id):
         return UserController.update_user(user_id)
 
-#-AQUI----------------------   REDIRECIONA PARA URL DA API
+#-----------------------   REDIRECIONA PARA URL DA API
     @app.route('/inicio')
     def inicio():
         return redirect(url_for("api"))    
-    
-    
-#-AQUI----------------------  URL PARA PERFIL COM O ID
+        
+#-----------------------  URL PARA PERFIL COM O ID
     @app.route('/perfil')
     @app.route('/perfil/<int:user_id>')
     def perfil(user_id=None):
-        return UserController.get_perfil(user_id)  
-      
-#-AQUI----------------------  ROTA DE LOGIN
-    @app.route('/login', methods=['POST'])
-    def login():
-        return UserController.login()
-
-    
+        return UserController.get_perfil(user_id) 
+             
 
         
 
